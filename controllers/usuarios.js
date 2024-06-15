d2daa8b1c1209d5e91bbdef470448ce05828e6c0
MATEUS APARECIDO PESSOA RODR <mateus.rodrigues64@etectupa.local>
2024-06-14 22:40:26 -0300

    testando api com banco de dados

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,20 +1,74 @@
 
 const db = require('../database/connection');
 
-module.experts = {
+module.exports = {
     async listarUsuarios(request, response){
         try{
+
+            // instruções SQL
+            const sql = `SELECT 
+            id, nome, email, ddd, telefone
+            FROM usuarios;`;
+            // executa instruções SQL e armazenao resultado na variavél usuários
+            const usuarios = await db.query(sql);
+
+
             return response.status(200).json({
                 sucesso: true,
-                menssagem: 'Lista de usuários.',
-                dados: null
+                menssagem: 'Listar Usuarios.',
+                dados: usuarios
             });
         } catch (error) {
-            return response.ststus(500).json({
+            return response.status(500).json({
                 sucesso: false,
                 message: 'Erro na requisição.',
                 dados: error.message
             })
         }
-    }
+    },
+    async cadastrarUsuarios(request, response){
+        try{
+            return response.status(200).json({
+                sucesso: true,
+                menssagem: 'usuario cadastrado',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                message: 'Erro na requisição.',
+                dados: error.message
+            })
+        }
+    },
+    async editarUsuarios(request, response){
+        try{
+            return response.status(200).json({
+                sucesso: true,
+                menssagem: 'Usuario editado.',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                message: 'Erro na requisição.',
+                dados: error.message
+            })
+        }
+    },
+    async apagarUsuarios(request, response){
+        try{
+            return response.status(200).json({
+                sucesso: true,
+                menssagem: 'Usuario deletado.',
+                dados: null
+            });
+        } catch (error) {
+            return response.status(500).json({
+                sucesso: false,
+                message: 'Erro na requisição.',
+                dados: error.message
+            })
+        }
+    },
 }
